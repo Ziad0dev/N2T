f406b02d3b1545bdaad471fda48f2ecae986418d
N3r0m5ns3r <66523061+N3r0m5ns3r@users.noreply.github.com>
2024-10-16 15:58:30 +0200

    assembler.py

---
+++ nand2tetris/projects/06/assembler/assembler.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3
+import Parser, Code, SymTable, sys
+
+class Assembler(object):
+    def __init__(self):
+        self.symb = SymTable,SymTable()
+        self.symb.addr = 16
+
+
+
