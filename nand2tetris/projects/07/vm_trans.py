616560976817a1efa5757360e22d7dcfd054d976
ziad0dev <66523061+Ziad0dev@users.noreply.github.com>
2025-09-08 10:01:21 +0200

    final version of the vm code

--- nand2tetris/projects/07/vm_trans.py
+++ nand2tetris/projects/07/vm_trans.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import glob
 
 class Parser:
     ARITHMETIC = {
@@ -72,12 +74,20 @@ class Parser:
 class CodeWriter:
     def __init__(self, output_file):
         self.file = open(output_file, "w")
-        self.filename = output_file.split(".")[0]
+        self.filename = None
         self.label_counter = 0
         self.current_function = None
         self.call_counter = 0
 
+    def set_filename(self, filename):
+        self.filename = filename
+
+    def write_bootstrap(self):
+        self.file.write("@256\nD=A\n@SP\nM=D\n")
+        self.write_call("Sys.init", 0)
+
     def write_arithmetic(self, command):
+        self.file.write(f"// {command}\n")
         if command == "add":
             self._write_binary("M=M+D")
         elif command == "sub":
@@ -98,34 +108,98 @@ class CodeWriter:
             self._write_unary("M=!M")
 
     def write_push_pop(self, command_type, segment, index):
+        self.file.write(f"// {command_type.lower()} {segment} {index}\n")
         if command_type == "C_PUSH":
             if segment == "constant":
                 self.file.write(f"@{index}\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
-            else:
-                base = self._segment_base(segment, index)
-                self.file.write(f"{base}D=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+            elif segment in ("local", "argument", "this", "that"):
+                base = self._segment_pointer(segment)
+                self.file.write(f"@{index}\nD=A\n{base}A=M+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+            elif segment == "pointer":
+                addr = "THIS" if int(index) == 0 else "THAT"
+                self.file.write(f"@{addr}\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+            elif segment == "temp":
+                self.file.write(f"@{5+int(index)}\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+            elif segment == "static":
+                self.file.write(f"@{self.filename}.{index}\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
         elif command_type == "C_POP":
-            base = self._segment_base(segment, index)
-            self.file.write(f"@SP\nM=M-1\nA=M\nD=M\n{base}M=D\n")
+            if segment in ("local", "argument", "this", "that"):
+                base = self._segment_pointer(segment)
+                self.file.write(f"@{index}\nD=A\n{base}D=M+D\n@R13\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D\n")
+            elif segment == "pointer":
+                addr = "THIS" if int(index) == 0 else "THAT"
+                self.file.write(f"@SP\nM=M-1\nA=M\nD=M\n@{addr}\nM=D\n")
+            elif segment == "temp":
+                self.file.write(f"@SP\nM=M-1\nA=M\nD=M\n@{5+int(index)}\nM=D\n")
+            elif segment == "static":
+                self.file.write(f"@SP\nM=M-1\nA=M\nD=M\n@{self.filename}.{index}\nM=D\n")
 
-    def _segment_base(self, segment, index):
-        # Returns assembly code to set A to the target address
-        if segment == "local":
-            return f"@LCL\nD=M\n@{index}\nA=D+A\n"
-        elif segment == "argument":
-            return f"@ARG\nD=M\n@{index}\nA=D+A\n"
-        elif segment == "this":
-            return f"@THIS\nD=M\n@{index}\nA=D+A\n"
-        elif segment == "that":
-            return f"@THAT\nD=M\n@{index}\nA=D+A\n"
-        elif segment == "pointer":
-            return f"@{'THIS' if int(index) == 0 else 'THAT'}\n"
-        elif segment == "temp":
-            return f"@{5+int(index)}\n"
-        elif segment == "static":
-            return f"@{self.filename}.{index}\n"
+    def write_label(self, label):
+        if self.current_function:
+            full_label = f"{self.current_function}${label}"
         else:
-            raise ValueError(f"Unknown segment: {segment}")
+            full_label = label
+        self.file.write(f"// label {label}\n")
+        self.file.write(f"({full_label})\n")
+
+    def write_goto(self, label):
+        if self.current_function:
+            full_label = f"{self.current_function}${label}"
+        else:
+            full_label = label
+        self.file.write(f"// goto {label}\n")
+        self.file.write(f"@{full_label}\n0;JMP\n")
+
+    def write_if(self, label):
+        if self.current_function:
+            full_label = f"{self.current_function}${label}"
+        else:
+            full_label = label
+        self.file.write(f"// if-goto {label}\n")
+        self.file.write("@SP\nM=M-1\nA=M\nD=M\n" + f"@{full_label}\nD;JNE\n")
+
+    def write_function(self, function_name, num_locals):
+        self.current_function = function_name
+        self.file.write(f"// function {function_name} {num_locals}\n")
+        self.file.write(f"({function_name})\n")
+        for i in range(int(num_locals)):
+            self.file.write("@SP\nA=M\nM=0\n@SP\nM=M+1\n")
+
+    def write_call(self, function_name, num_args):
+        return_label = f"RETURN_{self.call_counter}"
+        self.call_counter += 1
+        self.file.write(f"// call {function_name} {num_args}\n")
+        self.file.write(f"@{return_label}\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+        self.file.write("@LCL\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+        self.file.write("@ARG\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+        self.file.write("@THIS\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+        self.file.write("@THAT\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+        self.file.write(f"@SP\nD=M\n@{5 + int(num_args)}\nD=D-A\n@ARG\nM=D\n")
+        self.file.write("@SP\nD=M\n@LCL\nM=D\n")
+        self.file.write(f"@{function_name}\n0;JMP\n")
+        self.file.write(f"({return_label})\n")
+
+    def write_return(self):
+        self.file.write("// return\n")
+        self.file.write("@LCL\nD=M\n@R13\nM=D\n")
+        self.file.write("@5\nA=D-A\nD=M\n@R14\nM=D\n")
+        self.file.write("@SP\nM=M-1\nA=M\nD=M\n@ARG\nA=M\nM=D\n")
+        self.file.write("@ARG\nD=M+1\n@SP\nM=D\n")
+        self.file.write("@R13\nM=M-1\nA=M\nD=M\n@THAT\nM=D\n")
+        self.file.write("@R13\nM=M-1\nA=M\nD=M\n@THIS\nM=D\n")
+        self.file.write("@R13\nM=M-1\nA=M\nD=M\n@ARG\nM=D\n")
+        self.file.write("@R13\nM=M-1\nA=M\nD=M\n@LCL\nM=D\n")
+        self.file.write("@R14\nA=M\n0;JMP\n")
+
+    def _segment_pointer(self, segment):
+        if segment == "local":
+            return "@LCL\n"
+        elif segment == "argument":
+            return "@ARG\n"
+        elif segment == "this":
+            return "@THIS\n"
+        elif segment == "that":
+            return "@THAT\n"
 
     def _write_binary(self, op):
         self.file.write("@SP\nM=M-1\nA=M\nD=M\n@SP\nM=M-1\nA=M\n" + op + "\n@SP\nM=M+1\n")
@@ -137,123 +211,62 @@ class CodeWriter:
         label_true = f"TRUE_{self.label_counter}"
         label_end = f"END_{self.label_counter}"
         self.label_counter += 1
-        self.file.write(
-            "@SP\nM=M-1\nA=M\nD=M\n@SP\nM=M-1\nA=M\nD=M-D\n"
-            f"@{label_true}\nD;{jump}\n"
-            "@SP\nA=M\nM=0\n"
-            f"@{label_end}\n0;JMP\n"
-            f"({label_true})\n@SP\nA=M\nM=-1\n"
-            f"({label_end})\n@SP\nM=M+1\n"
-        )
-    def write_label(self, label):
-        self.file.write(f"// label {label}\n")
-        self.file.write(f"({label})\n")
-
-    def write_goto(self, label):
-        self.file.write(f"// goto {label}\n")
-        self.file.write(f"@{label})\n0;JMP\n")
-
-    def write_if(self, label):
-        self.file.write(f"// if-goto {label}\n")
-        self.file.write(
-            "@SP\n"
-            "M=M-1\n"
-            "A=M\n"
-            "D=M\n"
-            f"@{label}\n"
-            "D;JNE\n"
-        )
-    def write_function(self, function_name, num_locals):
-        self.current_function = function_name
-        self.file.write(f"// function {function_name} {num_locals}\n")
-        self.file.write(f"({function_name})\n")
-
-        for i in range(int(num_locals)):
-            self.file.write("@SP\n")
-            self.file.write("A=M\n")
-            self.file.write("M=0\n")
-            self.file.write("@SP\n")
-            self.file.write("M=M+1\n")
-
-    def write_call(self, function_name, num_args):
-
-        return_label = f"RETURN_{self.call_counter}"
-        self.call_counter += 1 
-        
-        self.file.write(f"// call {function_name} {num_args}\n")
-
-        self.file.write(f"@{return_label}\n")
-        self.file.write("D=A\n")
-        self.file.write("@SP\n")
-        self.file.write("A=M\n")
-        self.file.write("M=D\n")
-        self.file.write("@SP\n")
-        self.file.write("M=M+1\n")
-
-        self.file.write("@LCL\n")
-        self.file.write("D=M\n")
-        self.file.write("@SP\n")
-        self.file.write("A=M\n")
-        self.file.write("M=D\n")
-        self.file.write("@SP\n")
-        self.file.write("M=M+1\n")
-
-        self.file.write("@ARG\n")
-        self.file.write("D=M\n")
-        self.file.write("@SP\n")
-        self.file.write("A=M\n")
-        self.file.write("M=D\n")
-        self.file.write("@SP\n")
-        self.file.write("M=M+1\n")
-
-        self.file.write("@SP\n")
-        self.file.write("D=M\n")
-        self.file.write("@LCL\n")
-        self.file.write("M=D\n")
-
-        self.file.write(f"@{function_name}\n")
-        self.file.write("0;JMP\n")
-
-        self.file.write(f"({return_label})")
-
-    def write_return(self):
-        self.file.write("// return\n")
-
-        self.file.write("@LCL\n")
-        self.file.write("D=M\n")
-        self.file.write("@R13\n")
-        self.file.write("M=D\n")
-
-        self.file.write()
+        self.file.write("@SP\nM=M-1\nA=M\nD=M\n@SP\nM=M-1\nA=M\nD=M-D\n")
+        self.file.write(f"@{label_true}\nD;{jump}\n")
+        self.file.write("@SP\nA=M\nM=0\n")
+        self.file.write(f"@{label_end}\n0;JMP\n")
+        self.file.write(f"({label_true})\n@SP\nA=M\nM=-1\n")
+        self.file.write(f"({label_end})\n@SP\nM=M+1\n")
 
     def close(self):
         self.file.close()
 
 def main():
     if len(sys.argv) != 2:
-        print("Usage: python3 vm_trans.py MyProgram.vm")
+        print("Usage: python3 vmtranslator.py <input.vm or directory>")
         return
-    input_file = sys.argv[1]
-    output_file = input_file.replace(".vm", ".asm")
 
-    parser = Parser(input_file)
+    input_path = sys.argv[1]
+
+    if os.path.isdir(input_path):
+        vm_files = glob.glob(os.path.join(input_path, "*.vm"))
+        vm_files.sort()
+        output_file = os.path.join(input_path, os.path.basename(os.path.normpath(input_path)) + ".asm")
+        needs_bootstrap = True
+    else:
+        vm_files = [input_path]
+        output_file = input_path.replace(".vm", ".asm")
+        needs_bootstrap = False
+
     codewriter = CodeWriter(output_file)
+    if needs_bootstrap:
+        codewriter.write_bootstrap()
 
-    while parser.has_more_commands():
-        parser.advance()
-        ctype = parser.command_type()
-        if ctype == "C_ARITHMETIC":
-            codewriter.write_arithmetic(parser.arg1())
-        elif ctype in ("C_PUSH", "C_POP"):
-            codewriter.write_push_pop(ctype, parser.arg1(), parser.arg2())
-        elif ctype == "C_LABEL":
-            codewriter.write_label(parser.arg1())
-        elif ctype == "C_GOTO":
-            codewriter.write_goto(parser.arg1())
-        elif ctype == "C_IF":
-            codewriter.write_if(parser.arg1())
-    parser.close()
+    for vm_file in vm_files:
+        filename = os.path.basename(vm_file).replace(".vm", "")
+        codewriter.set_filename(filename)
+        parser = Parser(vm_file)
+        while parser.has_more_commands():
+            parser.advance()
+            ctype = parser.command_type()
+            if ctype == "C_ARITHMETIC":
+                codewriter.write_arithmetic(parser.arg1())
+            elif ctype in ("C_PUSH", "C_POP"):
+                codewriter.write_push_pop(ctype, parser.arg1(), parser.arg2())
+            elif ctype == "C_LABEL":
+                codewriter.write_label(parser.arg1())
+            elif ctype == "C_GOTO":
+                codewriter.write_goto(parser.arg1())
+            elif ctype == "C_IF":
+                codewriter.write_if(parser.arg1())
+            elif ctype == "C_FUNCTION":
+                codewriter.write_function(parser.arg1(), parser.arg2())
+            elif ctype == "C_CALL":
+                codewriter.write_call(parser.arg1(), parser.arg2())
+            elif ctype == "C_RETURN":
+                codewriter.write_return()
+        parser.close()
     codewriter.close()
 
 if __name__ == "__main__":
-    main()  
+    main()
